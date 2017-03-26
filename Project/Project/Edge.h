4b19125eccef8981e8c87462a7534b21ddcccbe1
Rhianne Hadfield <dawg_rh@hotmail.com>
2017-03-26 12:49:05 -0600

    Update Edge.h
    Added Comments

--- Project/Project/Edge.h
+++ Project/Project/Edge.h
@@ -1,3 +1,11 @@
+/*
+ Edge Class
+ 	Pair is a reference the adjcent edge object in the edge pair.
+	Next and Prev are references to edge ojbect that are connected to the current edge.
+	Vert is a reference to the edges's starting vertex
+	face is a refernece to face the edge is connected to.
+*/
+
 #ifndef EDGE_H
 #define EDGE_H
 #include <vector>
